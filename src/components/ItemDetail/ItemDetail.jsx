b44d9d54d695422a8eac93b44314f98cc035edf4
Rodrigo Facundo <frodzek@proton.me>
2023-11-08 17:08:56 -0300

    95%

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -4,7 +4,7 @@ const ItemDetail = ({ itemSelected}) => {
   return (
     <div>
       <h6 className="card-title">{itemSelected?.title}</h6>
-      <img src="{itemSelected?.image}" alt="{itemSelected??.title}" width={70}/>
+      <img className='imagen' src={itemSelected?.image} alt={itemSelected?.title} width={70}/>
       <div className="card-description">
         <p>{itemSelected?.description}</p>
 
--- src/pages/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/pages/ItemDetailContainer/ItemDetailContainer.jsx
@@ -3,18 +3,18 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 const ItemDetailContainer = () => {
-    const [product, productList] = useState({});
+    const [product, setProduct] = useState({});
     const { id } = useParams ();
 
-    const fetchProducts = () => {
-        fetch ('https://fakestoreapi.com/products/${id}')
+    const fetchProduct = () => {
+      fetch(`https://fakestoreapi.com/products/${id}`)
         .then ((response) => response.json())
-        .then ((data) => productList(data)) 
+        .then ((data) => setProduct(data)) 
         .catch ((error) => console.log(error));
     };
 
     useEffect(() => {
-        fetchProducts();
+        fetchProduct();
     }, []);
 
   return (
